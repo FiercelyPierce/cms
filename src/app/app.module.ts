342fa52ada60d1de117ba4ab7f003c0b1e4e30b8
Pierce Cirks <97271154+FiercelyPierce@users.noreply.github.com>
2025-02-10 20:25:06 -0700

    feat: refactor contact item component structure and move to contact list directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { HeaderComponent } from './header.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
-import { ContactItemComponent } from './contacts/contact-item/contact-item.component';
+import { ContactItemComponent } from './contacts/contact-list/contact-item/contact-item.component';
 import { DocumentsComponent } from './documents/documents.component';
 import { DocumentListComponent } from './documents/document-list/document-list.component';
 import { DocumentItemComponent } from './documents/document-list/document-item/document-item.component';
